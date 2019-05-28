9f6c5dd383051c230e922df8b178c284202f1215
kiran4454 <saikiranm.m@gmail.com>
2019-05-28 17:52:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '8NRBmdVWJBz6q0SPjDqusUQdm',
+    consumer_secret: 'F7fjMNt4gSJ2KFvzqeCaWBvknxQ9PVd6kH8ymoqZrp5ft1qJbM',
+    access_token_key: '1118026351787646976-eO0pTdYaRVju2yujKxJGczBiKxPXCK',
+    access_token_secret: 'ce9NsxUW1OLPtLPmPdDxwidSC22DiaycSDp4rFjsJKZzK'
+  }
