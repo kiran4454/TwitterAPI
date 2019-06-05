d63277b550604e74b01a36851721fc96abec5ae6
kiran4454 <saikiranm.m@gmail.com>
2019-06-05 12:18:03 +0530

    Add files via upload

---
+++ post.js
@@ -0,0 +1,17 @@
+var Twitter = require('twitter');
+var config = require('./config')
+var T = new Twitter(config);
+const express=require('express');
+const app=express();
+var tweet = {
+status: 'Trial!!' } // this is the tweet message
+T.post('statuses/update', tweet, tweeted) // this is how we actually post a tweet ,again takes three params 'statuses/update' , tweet message and a call back function
+function tweeted(err, data, response) {
+if(err){
+console.log("Something went wrong!");
+}
+else{
+console.log("It worked!");
+}
+} 
+app.listen(3000,()=>console.log('listening on port 3000'));
